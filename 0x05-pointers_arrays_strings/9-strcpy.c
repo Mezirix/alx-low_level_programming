880d2a932a3eb905c215f7e316257c49e0681fbb
Mezirix <chimezirianyanwus.co>
2022-06-13 19:47:17 -0700

    task 9

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -3,7 +3,7 @@
 /**
  * _strcpy - Copies a string pointed to by @src, including the
  *
- * terminating null byte, to a buffer pointed to by @dest.
+ * terminating null byte, to a buffer pointed to by @dest
  * @dest: A buffer to copy the string to.
  * @src: A source string to cpoy.
  *
@@ -12,8 +12,8 @@
 char *_strcpy(char *dest, char *src)
 {
 	int index = 0;
-		
-	for (index = 0; src[index] != '\0'; index++)	
+
+	for (index = 0; src[index] != '\0'; index++)
 	{
 		dest[index] = src[index];
 	}
