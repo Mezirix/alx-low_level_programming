f523fcc66e68c82369db9aba8a62067eda4567cd
Chimeziri Anyanwu <103989041+Mezirix@users.noreply.github.com>
2022-07-14 20:54:37 -0400

    Update lists.h

--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -1,4 +1,4 @@
-i#ifndef LISTS_H
+#ifndef LISTS_H
 #define LISTS_H
 
 /*
@@ -18,12 +18,12 @@ i#ifndef LISTS_H
  *
  * Description: singly linked list node structure
  *              for Holberton project
- *
  */
 typedef struct list_s
-{		char *str;
-			unsigned int len;
-				struct list_s *next;
+{
+	char *str;
+	unsigned int len;
+	struct list_s *next;
 } list_t;
 
 size_t print_list(const list_t *h);
