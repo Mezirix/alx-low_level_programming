464ab56f08ba80dd0efc456fee0d1a9f3c7a5151
Mezirix <chimeziri@anyanwus.com>
2022-07-21 16:30:58 -0700

    101-password

--- 0x14-bit_manipulation/100-get_endianness.c
+++ 0x14-bit_manipulation/100-get_endianness.c
@@ -1,22 +1,35 @@
 #include "main.h"
 
+
+
 /**
- * flip_bits - Counts the number of bits needed to be
- *             flipped to get from one number to another.
- * @n: The number.
- * @m: The number to flip n to.
  *
- * Return: The necessary number of bits to flip to get from n to m.
- */
-unsigned int flip_bits(unsigned long int n, unsigned long int m)
+ *  * get_endianness - Checks the endianness.
+ *
+ *   *
+ *
+ *    * Return: If big-endian - 0.
+ *
+ *     *         If little-endian - 1.
+ *
+ *      */
+
+int get_endianness(void)
+
 {
-	unsigned long int xor = n ^ m, bits = 0;
 
-	while (xor > 0)
-	{
-		bits += (xor & 1);
-		xor >>= 1;
-	}
+		int num = 1;
 
-	return (bits);
+			char *endian = (char *)&num;
+
+
+
+				if (*endian == 1)
+
+							return (1);
+
+
+
+					return (0);
+
 }
